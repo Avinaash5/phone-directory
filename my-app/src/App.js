fbb9f83f0f36cccb5ea34c7eec9ef16f2bbe4595
Avinash <origantiavinash@gmail.com>
2023-02-16 11:16:19 +0530

    Displayed Phone Directory Displayed Phone Directory

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
+import React, {Component} from "react";
 
-
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div >
+        Phone Directory
+      </div>
+    )
+  }
+ 
 }
 
 export default App;
